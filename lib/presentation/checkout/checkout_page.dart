b80592f270eebeaa01d63b4bc61b9be0d66e9e73
Windys <windysabtami85@gmail.com>
2023-09-15 16:27:40 +0800

    masih belum beres

--- lib/presentation/checkout/checkout_page.dart
+++ lib/presentation/checkout/checkout_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../bloc/checkout/checkout_bloc.dart';
+import '../../bloc/order/order_bloc.dart';
+import '../../data/datasources/auth_local_datasource.dart';
+import '../../data/models/order_request_model.dart';
 
 class CheckoutPage extends StatefulWidget {
   const CheckoutPage({super.key});
@@ -12,32 +18,126 @@ class _CheckoutPageState extends State<CheckoutPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Checkout Page')),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            children: [
-              const Text('Alamat Pengiriman'),
-              const SizedBox(
-                height: 8,
-              ),
-              TextField(
-                controller: addressController,
-                maxLines: 4,
-                style: const TextStyle(fontSize: 14),
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Alamat Pengiriman',
-                ),
-              ),
-              const SizedBox(
+      appBar: AppBar(title: const Text('Checkout Page')),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            const Text('Alamat Pengiriman'),
+            const SizedBox(
               height: 8,
             ),
-            //lanjutkan tampilan checkoutnya ?
-
-            
-            ],
+            TextField(
+              controller: addressController,
+              maxLines: 4,
+              style: const TextStyle(fontSize: 14),
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Alamat Pengiriman',
+              ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            BlocBuilder<CheckoutBloc, CheckoutState>(
+              builder: (context, state) {
+                if (state is CheckoutLoaded) {
+                  final uniqueItem = state.items.toSet().length;
+                  final dataSet = state.items.toSet();
+                  return Expanded(
+                    child: ListView.builder(
+                      itemBuilder: (context, index) {
+                        final count = state.items
+                            .where(
+                              (element) =>
+                                  element.id == dataSet.elementAt(index).id,
+                            )
+                            .length;
+                        return ListTile(
+                          leading: CircleAvatar(
+                              backgroundImage: NetworkImage(
+                                  dataSet.elementAt(index).attributes!.image!)),
+                          title: Text(
+                            dataSet.elementAt(index).attributes!.name!,
+                            style: const TextStyle(fontSize: 12),
+                          ),
+                          trailing: Text('$count'),
+                        );
+                      },
+                      itemCount: uniqueItem,
+                    ),
+                  );
+                }
+                return Expanded(
+                  child: ListView.builder(
+                    itemBuilder: (context, index) {
+                      return Container(
+                        color: Colors.red,
+                        child: const Text('ok'),
+                      );
+                    },
+                    itemCount: 0,
+                  ),
+                );
+              },
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: BlocListener<OrderBloc, OrderState>(
+          listener: (context, state) {
+            state.maybeWhen(
+              orElse: () {},
+              loaded: (model) {
+                // Navigator.push(context, MaterialPageRoute(builder: (context) {
+                //   return SnapWidget(url: model.redirectUrl);
+                // }));
+              },
+            );
+          },
+          child: BlocBuilder<CheckoutBloc, CheckoutState>(
+            builder: (context, state) {
+              if (state is CheckoutLoaded) {
+                return ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xffEE4D2D)),
+                  onPressed: () async {
+                    final userId = await AuthLocalDatasource().getUserId();
+                    final total = state.items
+                        .fold(0, (sum, item) => sum + item.attributes!.price!);
+                    final data = Data(
+                      items: state.items
+                          .map((e) => Item(
+                              id: e.id!,
+                              productName: e.attributes!.name!,
+                              price: e.attributes!.price!,
+                              qty: 1))
+                          .toList(),
+                      totalPrice: total,
+                      deliveryAddress: addressController.text,
+                      courierName: 'JNE',
+                      shippingCost: 22000,
+                      statusOrder: 'waitingPayment',
+                      userId: userId,
+                    );
+                    final requestModel = OrderRequestModel(data: data);
+                    context
+                        .read<OrderBloc>()
+                        .add(OrderEvent.doOrder(requestModel));
+                  },
+                  child: const Text(
+                    'Bayar',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                );
+              }
+              return const CircularProgressIndicator();
+            },
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
